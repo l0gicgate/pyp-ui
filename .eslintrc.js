3faa033c09c7f2e3dc3b3cfb56086d2f8b4228e7
Pierre Bérubé <pierre@lgse.com>
2022-02-07 00:47:32 -0700

    update to next 12

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
-    'jest/globals': true,
     browser: true,
     es6: true,
   },
-  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:jest/recommended'],
-  ignorePatterns: ['__mocks__/', 'node_modules/'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  ignorePatterns: ['node_modules/'],
   parser: '@babel/eslint-parser',
-  plugins: ['jest', 'module-resolver', 'prettier', 'react'],
+  plugins: ['prettier', 'react', 'sort-keys'],
+  root: true,
   rules: {
     'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0,
@@ -16,13 +16,13 @@ module.exports = {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'react/button-has-type': 0,
     'react/forbid-prop-types': 0,
-    'react/jsx-props-no-spreading': 0,
     'react/jsx-filename-extension': [
       1,
       {
         extensions: ['.js', '.jsx'],
       },
     ],
+    'react/jsx-props-no-spreading': 0,
     'react/jsx-sort-props': [
       'error',
       {
@@ -32,11 +32,11 @@ module.exports = {
     ],
     'react/no-array-index-key': 0,
     'react/prefer-stateless-function': 0,
+    'sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, minKeys: 2, natural: false },
+    ],
+    'sort-keys/sort-keys-fix': 1,
   },
-  settings: {
-    'import/resolver': {
-      'babel-module': {},
-    },
-  },
-  root: true,
 };
--- next.config.js
+++ next.config.js
@@ -1,31 +1,17 @@
 require('dotenv').config();
 
-const withCss = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
-
-const workBoxOptions = {
+module.exports = {
+  dontAutoRegisterSw: true,
+  generateInDevMode: true,
+  generateSw: false,
+  globDirectory: '.',
+  globPatterns: ['static/**/*'],
+  images: {
+    domains: [process.env.PUBLIC_SITE_URL],
+  },
   workboxOpts: {
-    exclude: [/.+error\.js$/, /\.map$/],
+    workboxOpts: {
+      exclude: [/.+error\.js$/, /\.map$/],
+    },
   },
 };
-
-const backendHostname = new URL(process.env.PUBLIC_SITE_URL).hostname;
-
-module.exports = withCss(
-  withSass({
-    workboxOpts: {
-      workboxOpts: {
-        exclude: [/.+error\.js$/, /\.map$/],
-      },
-    },
-    generateInDevMode: true,
-    dontAutoRegisterSw: true,
-    generateSw: false,
-    globPatterns: ['static/**/*'],
-    globDirectory: '.',
-    target: 'serverless',
-    images: {
-      domains: [backendHostname, process.env.PUBLIC_SITE_URL],
-    },
-  })
-);
